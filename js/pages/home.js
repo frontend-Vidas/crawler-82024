90e173a689dc0435097dbd5ab856c2dfdd3fe445
tropikalis <desertukas@yahoo.com>
2024-08-14 08:43:38 +0300

    update

---
+++ js/pages/home.js
@@ -0,0 +1,21 @@
+// IMPORTS
+
+// EXECUTION
+
+/* HEADER: end */ 
+/* HEADER: start */ 
+
+/* HERO: end */ 
+/* HERO: start */ 
+
+/* CHOOSE US: start */ 
+/* CHOOSE US: start */ 
+
+/* METRICS: start */ 
+/* METRICS: end */ 
+
+/* LATEST VIDEO: start */ 
+/* LATEST VIDEO: end */ 
+
+/* FOOTER: start */ 
+/* FOOTER: end */ 
